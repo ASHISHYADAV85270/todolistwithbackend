da6c0171e50f36f5ffe322c4d82e888574cec03b
Ashish <savitayadavmansarover@gmail.com>
2023-07-05 10:39:20 +0530

    final touch to deployment

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ mongoose.set('strictQuery', false);
 export const connectDB = () => {
     mongoose
         .connect(process.env.MONGO_URL, { dbName: "TodoList", })
-        .then(() => { console.log("data base connected") })
+        .then((c) => { console.log(`data base connected with ${c.connection.host}`) })
         .catch((err) => { console.log(err) });
 }
